ccc6de54864545e389b4d2a89597cf2633164d87
carlito1225 <105741671+carlito1225@users.noreply.github.com>
2022-06-08 21:37:43 +0800

    Add files via upload

---
+++ day9/修改員工薪資.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+id = int(input("請輸入員工編號:"))
+esalary = int(input("請輸入員工修改後的薪資:"))
+sql='''
+    update employee set esalary = %d where id = %d 
+'''
+
+sql = sql.strip() % (esalary, id)
+print(sql)
+
+conn = sqlite3.connect('demo.db')
+cursor = conn.cursor()
+cursor.execute(sql)
+rowcount = cursor.rowcount  # 異動筆數
+print('新增', rowcount, '筆成功')
+conn.commit()  # 提交
+conn.close()
---
+++ day9/新增員工資料.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+id = int(input("請輸入員工編號:"))
+ename = input("請輸入員工姓名:")
+esalary = int(input("請輸入員工薪資:"))
+did = int(input("請輸入部門編號(101,201,301):"))
+sql = """
+    insert into employee(id, ename, esalary, did)
+    values(%d, "%s", %d, %d)
+"""
+
+sql = sql.strip() % (id, ename, esalary, did)
+print(sql)
+
+conn = sqlite3.connect('demo.db')
+cursor = conn.cursor()
+cursor.execute(sql)
+rowcount = cursor.rowcount  # 異動筆數
+print('新增', rowcount, '筆成功')
+conn.commit()  # 提交
+conn.close()
