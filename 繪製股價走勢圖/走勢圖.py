acd596ce3dc6b9a29a85d12900cc88dde6f16b6e
carlito1225 <105741671+carlito1225@users.noreply.github.com>
2022-08-10 21:32:58 +0800

    Add files via upload

---
+++ 繪製股價走勢圖/走勢圖.py
@@ -0,0 +1,45 @@
+'''
+https://pypi.org/project/mplfinance/
+安裝 pip install --upgrade mplfinance
+'''
+import sqlite3
+import pandas as pd
+import mplfinance as mpf
+
+def save_candle_chart(symbol, amount, savefile, mydpi=135, mav=(3, 6, 9)):
+    # 資料庫路徑
+    db_path = '../資料庫/財經資料庫.db'
+    conn = sqlite3.connect(db_path)
+    # 查找某一檔股票最近n天的股價資料
+    sql = '''
+        select 交易日 as Date, 開盤價 as Open, 最高價 as 'High',
+               最低價 as Low, 收盤價 as Close, 成交金額 as Volume
+        from price
+        where 證券代號 = '%s' order by 交易日 desc limit %s        
+    ''' % (symbol, amount)
+    print(sql)
+    # 將資料讀進 DataFrame
+    data = pd.read_sql(sql, conn)
+    data = data.sort_index(ascending=False)  # 根據 index 由大到小
+    data.index = pd.DatetimeIndex(data['Date'])
+    print(data)
+
+    # mpf.plot(data)
+    # mpf.plot(data, type='candle')
+    # mpf.plot(data, type='candle', mav=(3, 6, 9))
+    # mpf.plot(data, type='candle', mav=(3, 6, 9), volume=True)
+    myfigsize = (800/mydpi, 600/mydpi)
+    if savefile == True:
+        mpf.plot(data, type='candle', mav=mav, volume=True, figsize=myfigsize, savefig='chart.png')
+    else:
+        mpf.plot(data, type='candle', mav=mav, volume=True, figsize=myfigsize)
+
+
+
+if __name__ == '__main__':
+    #save_candle_chart('2330', 20, True)
+    #save_candle_chart('2330', 20, False)
+    #save_candle_chart('2330', 20, False, 200)
+    #save_candle_chart('2330', 20, False, 200, (3, 6))
+    #save_candle_chart('2330', 20, False, mav=(3, 6))
+    save_candle_chart('2330', 20, False, mydpi=100, mav=(3, 6))
---
+++ 繪製股價走勢圖/走勢圖GUI.py
@@ -0,0 +1,56 @@
+import tkinter
+from tkinter import font
+from PIL import Image, ImageTk
+import sqlite3
+import pandas as pd
+import mplfinance as mpf
+
+from 繪製股價走勢圖.走勢圖 import save_candle_chart
+
+def draw_chart():
+    save_candle_chart(symbol.get(), amount.get(), True, mydpi=100, mav=(3,6,9))
+    # 將chart.png 放入到 chartLabel 中
+    photo = ImageTk.PhotoImage(Image.open('chart.png'))
+    chartLabel.config(image=photo)
+    chartLabel.image = photo
+
+def exit_form():
+    root.destroy()
+    root.quit()
+
+if __name__ == '__main__':
+    # save_candle_chart('2330', 20, False, mydpi=100, mav=(3, 6))
+    root = tkinter.Tk()
+    root.geometry("800x600")
+    root.title("股價走勢圖")
+
+    myfont = font.Font(family='Arial', size=15, weight='bold')
+    myfont2 = font.Font(family='Arial', size=15)
+
+    symbol = tkinter.StringVar()
+    symbol.set('2330')
+    amount = tkinter.IntVar()
+    amount.set('20')
+
+    # view 元件
+    symbolLabel = tkinter.Label(root, text='股票代號', font=myfont)
+    symbolEntry = tkinter.Entry(root, textvariable=symbol, font=myfont2)
+    amountLabel = tkinter.Label(root, text='交易天數', font=myfont)
+    amountEntry = tkinter.Entry(root, textvariable=amount, font=myfont2)
+    drawButton = tkinter.Button(root, text='繪圖', command=lambda: draw_chart(), font=myfont)
+    exitButton = tkinter.Button(root, text='離開',command=lambda: exit_form(), font=myfont)
+    chartLabel = tkinter.Label(root, font=myfont)
+
+    root.rowconfigure(1, weight=1)
+    root.columnconfigure((4, 5), weight=1)
+
+    # view 元件布局
+    symbolLabel.grid(row=0, column=0, sticky='EWNS')
+    symbolEntry.grid(row=0, column=1, sticky='EWNS')
+    amountLabel.grid(row=0, column=2, sticky='EWNS')
+    amountEntry.grid(row=0, column=3, sticky='EWNS')
+    drawButton.grid(row=0, column=4, sticky='EWNS')
+    exitButton.grid(row=0, column=5, sticky='EWNS')
+    chartLabel.grid(row=1, column=0, columnspan=6, sticky='EWNS')
+
+    root.mainloop()
